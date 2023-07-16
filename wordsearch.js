95ecebca72347c4ead8a7eece90c7fe375206f61
India Cornell <indiaccornell@gmail.com>
2023-07-16 14:26:24 -0700

    update code and test code for pair programming answers

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    if(letters.length < 1 || word.length < 1) {
+        return false; 
+    } 
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = letters[0].map((col, index) => 
+        letters.map(row => row[index])
+        );
+    const verticalMap = verticalJoin.map((ls) => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+            if (l.includes(word)) {
+                console.log(`the word ${word} is present and horizontal`);
+                return true;
+            } else {
+        for (l of verticalMap) {
+            if (l.includes(word)) {
+                console.log(`the word ${word} is present and vertical`);
+                return true;
+            }
+        }    
     }
 }
+console.log(`the word ${word} is not present`);
+return false; 
+};
 
 module.exports = wordSearch
